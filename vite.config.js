4380be313c5e134bea199c658670e496d8425b79
Nico <akebra77@gmail.com>
2024-05-24 09:07:03 +0200

    vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+  },
+  server: {
+    open: true,
   },
 });
